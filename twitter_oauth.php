8c420faeeddd0dce5e3bcd906929f902e3c579aa
Brenda <brendateruel@aol.com>
2012-03-19 21:35:56 -0500

    trying out net.tutsplus' twitter oauth PHP tutorial using twitteroauth library by abraham

---
+++ twitter_oauth.php
@@ -0,0 +1,25 @@
+﻿<?php
+
+require("twitteroauth/twitteroauth.php");
+session_start();
+
+if(!empty($_GET['oauth_verifier']) && !empty($_SESSION['oauth_token']) && !empty($_SESSION['oauth_token_secret'])){  
+    // We've got everything we need  
+} else {  
+    // Something's missing, go back to square 1  
+    header('Location: twitter_login.php');  
+}  
+
+// TwitterOAuth instance, with two new parameters we got in twitter_login.php  
+$twitteroauth = new TwitterOAuth('JlVI5JLcRgiHOX8LhUhCmw', 'AzFZeQZ7zhhN397iLSPNC36MUryUBPufvqt8i1NI', $_SESSION['oauth_token'], $_SESSION['oauth_token_secret']);  
+// Let's request the access token  
+$access_token = $twitteroauth->getAccessToken($_GET['oauth_verifier']); 
+// Save it in a session var 
+$_SESSION['access_token'] = $access_token; 
+// Let's get the user's info 
+$user_info = $twitteroauth->get('account/verify_credentials'); 
+print "this worked";
+// Print user's info  
+print_r($user_info);  
+
+?>
