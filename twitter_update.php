92853540a155d5d2d12c95f672d40e98a818f2af
Brenda <brendateruel@aol.com>
2012-03-24 17:59:05 -0500

    added twitter_update.php file

---
+++ twitter_update.php
@@ -0,0 +1,21 @@
+﻿<?php
+
+require("twitteroauth/twitteroauth.php");
+session_start();
+
+print 'check the db now';
+
+if(!empty($_SESSION['username'])){  
+    $twitteroauth = new TwitterOAuth('JlVI5JLcRgiHOX8LhUhCmw', 'AzFZeQZ7zhhN397iLSPNC36MUryUBPufvqt8i1NI', $_SESSION['oauth_token'], $_SESSION['oauth_secret']);  
+}  
+
+$home_timeline = $twitteroauth->get('statuses/home_timeline');  
+print_r($home_timeline);
+
+?>
+
+<html>
+
+<h2>Hello <?=(!empty($_SESSION['username']) ? '@' . $_SESSION['username'] : 'Guest'); ?></h2>
+
+</html>
