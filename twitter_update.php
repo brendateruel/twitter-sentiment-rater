b71741e16d1ec7ff4e597c42bb086038791286ed
Brenda <brendateruel@aol.com>
2012-04-01 14:04:40 -0500

    updated INSERT queries to write into updated temp_timeline and new friends tables. foreach print function is displaying same tweet over and over again

--- twitter_update.php
+++ twitter_update.php
@@ -18,10 +18,13 @@ $home_timeline = $twitteroauth->get('statuses/home_timeline', array('count' => 2
 foreach($home_timeline->status as $status) {
 	$user = $status->user;
 	$date_time = date("Y-m-d H:i:s", strtotime($status->created_at)); 
-	$query = mysql_query("INSERT INTO temp_timeline (user_handle, user_image_URL, status_id, date_time, tweet) VALUES ('{$user->screen_name}', '{$user->profile_image_url}', '{$status->id}', '{$date_time}', '{$status->text}')");  
-	$query = mysql_query("SELECT * FROM temp_timeline");  
+	$query = mysql_query("INSERT INTO friends (user_handle, user_image_URL) VALUES ('{$user->screen_name}', '{$user->profile_image_url}')");  
+	$query = mysql_query("INSERT INTO temp_timeline (user_handle, status_id, date_time, tweet) VALUES ('{$user->screen_name}', '{$status->id}', '{$date_time}', '{$status->text}')");  
+	$query = mysql_query("SELECT * FROM friends");  
 	$result = mysql_fetch_array($query);
 	print_r($result);
+	mysql_free_result($query);
+	mysql_free_result($result);
 }
 
 ?>
