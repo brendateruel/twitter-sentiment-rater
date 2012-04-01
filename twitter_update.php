f9f4156bb648efbce9d50ca2eba659ac661df412
Brenda <brendateruel@aol.com>
2012-04-01 15:17:32 -0500

    now printing user_handle only but format is still off and seeing resource id #5 error. Also added more descriptive copy about what is happening and a link to my db-test.php file

--- twitter_update.php
+++ twitter_update.php
@@ -6,7 +6,7 @@ mysql_select_db('twitter_sentiment');
 require("twitteroauth/twitteroauth-xml.php");
 session_start();
 
-print 'check the db now';
+echo "You've been authenticated. Now let's import your friends and home_timeline!\r\n";
 
 if(!empty($_SESSION['username'])){  
     $twitteroauth = new TwitterOAuth('JlVI5JLcRgiHOX8LhUhCmw', 'AzFZeQZ7zhhN397iLSPNC36MUryUBPufvqt8i1NI', $_SESSION['oauth_token'], $_SESSION['oauth_secret']);  
@@ -20,18 +20,30 @@ foreach($home_timeline->status as $status) {
 	$date_time = date("Y-m-d H:i:s", strtotime($status->created_at)); 
 	$query = mysql_query("INSERT INTO friends (user_handle, user_image_URL) VALUES ('{$user->screen_name}', '{$user->profile_image_url}')");  
 	$query = mysql_query("INSERT INTO temp_timeline (user_handle, status_id, date_time, tweet) VALUES ('{$user->screen_name}', '{$status->id}', '{$date_time}', '{$status->text}')");  
-	$query = mysql_query("SELECT * FROM friends");  
-	$result = mysql_fetch_array($query);
-	print_r($result);
 	mysql_free_result($query);
-	mysql_free_result($result);
 }
+
+print 'The most recent tweets from your timeline have been added to our database.';
 
 ?>
 
 <html>
 
 <h2>Hello <?=(!empty($_SESSION['username']) ? '@' . $_SESSION['username'] : 'Guest'); ?></h2>
-<p></p>
+
+<p>
+<?= 
+	$query = mysql_query("SELECT user_handle FROM friends");  
+	$a = array();
+	while ($result = mysql_fetch_assoc($query)) {
+		$a[] = $result;
+	}
+	print_r($a);
+?>
+</p>
+
+<p>
+When you're ready, <a href="db-test.php">let's analyze those tweets!</a>
+</p>
 
 </html>
